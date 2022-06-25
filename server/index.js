b981240ac55b7b4b82a2c3f179dcc60d338fcb85
Emre Cakmak <emrecakmak93@hotmail.co.uk>
2022-06-25 15:48:13 +0100

    Add connection to MongoDB projectMS_db database and config file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const colors = require('colors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
-const app = express()
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
+const connectDB = require('./config/db')
+
+const app = express()
+
+//Connect to MongoDB Database
+connectDB()
 
 app.use('/graphql', graphqlHTTP({
     schema,
